cb71f41242cf48073973e2a09e23ed57173307c2
KShih <kontonsine@gmail.com>
2021-09-20 08:14:16 -0400

    [add] add setter in page.py

--- src/page.py
+++ src/page.py
@@ -8,7 +8,7 @@ class Page:
 
     url_o = urlparse(url)
     self.url = url_o.geturl()
-    self.domain = url_o.scheme + "://" + url_o.hostname # TODO: if hostname not found?
+    self.domain = url_o.scheme + "://" + url_o.hostname
     self.size = None
     self.depth = depth
     self.download_time = None
@@ -16,9 +16,19 @@ class Page:
   def __lt__(self, other):
     return self.total_score < other.total_score
 
-  def set_novel_score(self, domain_cnt):
+  def set_download_time(self, time):
+    self.download_time = time
+  
+  def set_page_size(self, page_size):
+    self.size = page_size
+
+  def update_importance_score(self):
+    self.importance_score += 1.0
+    self.update_total_score()
+
+  def update_novel_score(self, domain_cnt):
     self.novel_score = 1 / domain_cnt
     self.update_total_score()
 
   def update_total_score(self):
-    self.total_score = self.novel_score + self.importance_score
+    self.total_score = - (self.novel_score + self.importance_score) # make minHeap works as maxHeap
