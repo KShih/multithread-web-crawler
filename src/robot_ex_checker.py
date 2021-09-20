0f125e8c556557a4e4f325e164a7eebe43b7cd07
KShih <kontonsine@gmail.com>
2021-09-20 08:15:43 -0400

    [update] use different robot instance for diff url

--- src/robot_ex_checker.py
+++ src/robot_ex_checker.py
@@ -1,33 +1,28 @@
 from urllib.robotparser import RobotFileParser
+from urllib.parse import urlparse
+import urllib.error
+import constant
 
 class RobotExChecker:
   def __init__(self):
-    self.cached = {}
-    self.rp = RobotFileParser()
+    self.robot_cached = {}
 
-  def can_parse(self, url_obj):
+  def can_parse(self, url):
+    url_obj = urlparse(url)
     robot_url = self._get_robot_url(url_obj)
     request_url = url_obj.geturl()
-    if request_url not in self.cached:
-      self.rp.set_url(robot_url)
-      self.rp.read()
-      self.cached[request_url] = self.rp.can_fetch("*", request_url)
-    return self.cached[request_url]
-  
+    if robot_url not in self.robot_cached:
+      rp = RobotFileParser()
+      rp.set_url(robot_url)
+      try:
+        rp.read() # TODO(better): default timeout is 60s, maybe too long
+      except Exception as exception:
+        if type(exception) not in {urllib.error.URLError, UnicodeDecodeError}:
+          print(constant.WARNING_EXCEPTION_NOT_RECOGNIZED, exception)
+          # TODO: better dealing strategy
+        return True 
+      self.robot_cached[robot_url] = rp
+    return self.robot_cached[robot_url].can_fetch("*", request_url)
+
   def _get_robot_url(self, url_obj):
     return url_obj.scheme + "://" + url_obj.netloc + "/robots.txt"
-
-
-if __name__ == "__main__":
-  from urllib.parse import urlparse
-  test1 = "http://www.musi-cal.com/cgi-bin/search?city=San+Francisco"
-  test2 = "https://en.wikipedia.org/wiki/Dog"
-  test3 = "http://www.musi-cal.com/wp-admin/search?city=San+Francisco"
-  test1_o = urlparse(test1) # True
-  test2_o = urlparse(test2) # True
-  test3_o = urlparse(test3) # False
-
-  rb = RobotExChecker()
-  print(rb.can_parse(test1_o))
-  print(rb.can_parse(test2_o))
-  print(rb.can_parse(test3_o))
