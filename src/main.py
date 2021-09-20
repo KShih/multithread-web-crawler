77e96b3cfb2e4ff4c0a99a3fefe8f0d8671d290c
KShih <kontonsine@gmail.com>
2021-09-20 08:09:26 -0400

    [add] add timer function in main

--- src/main.py
+++ src/main.py
@@ -1,17 +1,20 @@
 from crawler import Crawler
-from page import Page
-from robot_ex_checker import RobotExChecker
 import os
+import time
 
 if __name__ == "__main__":
   crawler = Crawler()
-  crawler.set_query_word("dog")
+  crawler.set_query_word("cats")
   crawler.set_seed_size(10)
-  crawler.set_thread_pool_size(3)
-  crawler.set_output_path(os.path.abspath("playground/output"))
-  crawler.set_max_page(10000)
+  crawler.set_thread_pool_size(20)
+  crawler.set_output_path("playground/output")
+  crawler.set_max_page(100)
 
+  start = time.time()
   try:
     crawler.run()
   except Exception as error:
     print('Catch error in run(): ' + repr(error))
+  finally:
+    crawler.stop()
+  print(time.time() - start)
